34260558687cc757357f31a4f3fe751bccfd2a60
Ярослав <axegame3310@gmail.com>
2026-06-08 23:19:51 +0800

    Update 6-0-8.py

--- projects_6/task_6_0/6-0-8.py
+++ projects_6/task_6_0/6-0-8.py
@@ -1,6 +1,6 @@
 import pandas as pd
-with open("C:/Users/Даша/Desktop/6/6_0_8.txt", "w") as f:
-    df = pd.read_csv("C:/Users/Даша/Downloads/wild_boars.csv")
+with open("C:/Users/kruto/Desktop/6/6_0_8.txt", "w") as f:
+    df = pd.read_csv("C:/Users/kruto/Downloads/wild_boars.csv")
     std = df.groupby('gender')['tusk_length_cm'].std()
     sr = df.groupby('gender')['tusk_length_cm'].mean()
     cv = std/sr * 100

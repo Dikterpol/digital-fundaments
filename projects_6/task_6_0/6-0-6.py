042913447d20bf7c2198ea90b3010e3d5659a527
Ярослав <axegame3310@gmail.com>
2026-06-08 23:21:28 +0800

    Update 6-0-6.py

--- projects_6/task_6_0/6-0-6.py
+++ projects_6/task_6_0/6-0-6.py
@@ -1,6 +1,6 @@
 import pandas as pd
-with open("C:/Users/Даша/Desktop/6/6_0_6.txt", "w") as f:
-    df = pd.read_csv("C:/Users/Даша/Downloads/wild_boars.csv")
+with open("C:/Users/kruto/Desktop/6/6_0_6.txt", "w") as f:
+    df = pd.read_csv("C:/Users/kruto/Downloads/wild_boars.csv")
     q1 = df.groupby('gender')['length_cm'].quantile(0.25)
     q3 = df.groupby('gender')['length_cm'].quantile(0.75)
     iqr = q3-q1

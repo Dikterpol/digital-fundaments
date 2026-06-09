2e361dfc9bf3b015f0dd6c0590ab13cb24e8d999
Ярослав <axegame3310@gmail.com>
2026-06-09 16:01:00 +0300

    Update task_2-7_1.py

--- task_2_7/task_2-7_1.py
+++ task_2_7/task_2-7_1.py
@@ -1,13 +1,8 @@
-from datetime import datetime
-import random
-
 files = ["seq1", "seq2", "seq3", "seq4"]
-
-now = str(datetime.now())
 
 for name in files:
 
-   new_name = name + ".fasta" + "\t" + now[:10] + " " + str(int(now[11:13]) - random.randint(0, 2)) + ":" + str(int(now[14:16]) - random.randint(0, 2)) + ":" + str(int(now[17:19]) - random.randint(0, 2))
+   new_name = name + ".fasta"+ ".17.02.26"
 
    print(f"{new_name}")
 
